b268e3c9957936e6664c92d77d1d74381f205c0e
Alex Alemi <alexalemi@gmail.com>
2015-07-12 17:26:02 -0700

    with second test, simple working

--- battleship.py
+++ battleship.py
@@ -2,6 +2,10 @@
 The main battleship game logic container
 """
 
+# FIX FOR PYPY
+# import sys
+# sys.path.append("/home/alemi/anaconda/lib/python2.7/site-packages/")
+
 import os
 from collections import Counter
 import sys
@@ -14,7 +18,7 @@ ROOT = os.path.realpath(os.path.dirname(sys.argv[0]))
 
 class Process(object):
     """ A small wrapper to abstract the interaction with the process """
-    def __init__(self,path,*args,**kwargs):
+    def __init__(self, path, *args,**kwargs):
         self.shortname = os.path.basename(path)
         logging.debug("Initializing process for %s, path:%s", self.shortname, path)
         self.p = pexpect.spawn(path,*args,**kwargs)
@@ -31,8 +35,20 @@ class Process(object):
         logging.debug("Retrieved %r from %s", line, self.shortname)
         return line
 
+    def readguess(self, *args, **kwargs):
+        """ Read a guess from the player, report as tuple """
+        line = self.p.readline(*args, **kwargs)
+        logging.debug("Got raw guess line %s", line)
+        guess = tuple(map(int, line.strip().split(",")))
+        logging.info("%s guessed %r", self.shortname, guess)
+        return guess
+
+    def sendguess(self, guess):
+        self.sendline("{},{}".format(*guess))
+
     def readboard(self, *args, **kwargs):
         """ Read an entire board """
+        logging.debug("Attempting to read a board from %s", self.shortname)
         boardstrings = []
         for i in xrange(10):
             boardstrings.append( self.readline().strip() )
@@ -52,31 +68,67 @@ class TimeoutError(Exception):
     def __init__(self, player):
         self.player = player
 
+
+class BattleshipPlayer(object):
+    """ A small container for the data
+    associated with a player """
+    def __init__(self, process, opponent, playernum):
+        self.name = process
+        self.opponent = opponent
+        self.playernum = playernum
+        self.p = None
+        self.board = {}
+        self.guesses = set()
+        self.lives = {"A" : 5, "B": 4, "D": 3, "S": 3, "P": 2}
+
+        self.initialize_process()
+
+    def initialize_process(self):
+        # Hold the popen objects for the two players
+        self.p = Process(os.path.join(ROOT, self.name))
+        self.p.delaybeforesend = 0
+        #Try to tell each of them their opponents name
+        self.p.sendline("{}, {}".format(self.playernum, self.opponent))
+
+    def read_board(self):
+        self.board = self.p.readboard()
+
+    @property
+    def alive(self):
+        """ Check to see if the player is still alive """
+        if any(self.lives.values()):
+            return True
+        return False
+
+
 class BattleshipGame(object):
     """ A single instance of a battleship game """
 
     def __init__(self, opp0, opp1):
+
         if random.random() < 0.5:
-            self.opp0 = opp0
-            self.opp1 = opp1
             self.switch = False
         else:
-            self.opp0 = opp1
-            self.opp1 = opp0
+            opp0, opp1 = opp1, opp0
             self.switch = True
-        
-        # the boards for the two players
-        self.board0 = {}
-        self.board1 = {}
+        self.p0 = BattleshipPlayer(opp0, opp1, 0)
+        self.p1 = BattleshipPlayer(opp1, opp0, 1)
 
+        self.turns = 0
+
+        self.guesser = self.p0
+        self.marker = self.p1
+
+        self.finished = False
         self.winner = 0.5
-
 
     def _gameinit(self):
         """ Initialize a game and catch errors """
         try:
-            self._initialize_processes()
-            self._read_boards()
+            self.p0.initialize_process()
+            self.p1.initialize_process()
+            self.p0.read_board()
+            self.p1.read_board()
             self._validate_boards()
         except BoardError as e:
             self.winner = 1-e.player
@@ -86,41 +138,81 @@ class BattleshipGame(object):
             return 1
         return 0
 
+    def turn(self):
+        # main game loop
+        self.turns += 1
+        logging.debug("Entering turn %d", self.turns)
+
+        # need to read a guess from the guesser
+        guess = self.guesser.p.readguess()
+        # send the guess to the marker
+        self.marker.p.sendguess(guess)
+
+        # check to see if this is a hit
+        ship = self.marker.board.get(guess, '')
+        if ship:
+            # we have a hit, if this hasn't been
+            # guessed before, reduce the lives of the ship
+            # by one
+            if guess not in self.guesser.guesses:
+                self.guesser.guesses.add(guess)
+                self.marker.lives[ship] -= 1
+                # check to see if we've killed the ship
+                if self.marker.lives[ship] == 0:
+                    # we've killed the ship, check to see if we've won
+                    if not self.marker.alive:
+                        # Just won.
+                        logging.info("Won the game!")
+                        self.guesser.p.sendline("W")
+                        self.winner = 1 - self.turns % 2
+                        self.finished = True
+                        return
+                    else:
+                        # sunk a ship, but not dead yet
+                        logging.info("Sunk the %r ship", ship)
+                        self.guesser.p.sendline("S{}".format(ship))
+                else:
+                    # just got a hit
+                    logging.info("Registered a hit")
+                    self.guesser.p.sendline("H")
+            else:
+                # a hit, but already counted
+                logging.info("Registered a REPEATED hit")
+                self.guesser.p.sendline("H")
+        else:
+            # we had a miss, send a miss
+            logging.info("Miss")
+            self.guesser.p.sendline("M")
+       
+        # swap players
+        self.guesser, self.marker = self.marker, self.guesser
 
     def game(self):
         """ Run a game from start to finish """
+        logging.info("Running a full game.")
         self._gameinit()
 
-
+        while not self.finished:
+            self.turn()
 
         return self.winner if not self.switch else 1-self.winner
 
-    def _initialize_processes(self):
-        # Hold the popen objects for the two players
-        self.p0 = Process(os.path.join(ROOT, self.opp0))
-        self.p1 = Process(os.path.join(ROOT, self.opp1))
-
-        #Try to tell each of them their opponents name
-        self.p0.sendline("0, {}".format(self.opp1))
-        self.p1.sendline("1, {}".format(self.opp0))
-
-    def _read_boards(self):
-        self.board0 = self.p0.readboard()
-        self.board1 = self.p1.readboard()
 
     def _validate_boards(self):
-        """ Check to see if the board is valid """
+        """ Check to see if the board is valid
+        raise an error if there is a problem with the board"""
         validcount = Counter("AAAAABBBBSSSDDDPP")
 
-        count0 = Counter(self.board0.values())
+        count0 = Counter(self.p0.board.values())
         if count0 != validcount:
             logging.error("ERROR on player 0 board")
             raise BoardError(0)
 
-        count1 = Counter(self.board1.values())
+        count1 = Counter(self.p1.board.values())
         if count1 != validcount:
             logging.error("ERROR on player 1 board")
             raise BoardError(1)
         
 if __name__ == "__main__":
-    Q = BattleshipGame('test.py','test_bad.py')
+    Q = BattleshipGame('test.py','test2.py')
+    print Q.game()
---
+++ test2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import logging
+from random import randrange
+logging.basicConfig(filename="logs/test.py", level=logging.DEBUG)
+
+# first we recieve an init string
+initstring = raw_input()
+turn, opponent = initstring.split(",")
+
+if turn=="0":
+    myturn = True
+else:
+    myturn = False
+logging.debug("initstring: %s", initstring)
+
+#now we need to send a board
+board = ["0000000000", 
+        "00000000PP",
+        "00B0000000",
+        "00B000A000",
+        "00B000ASSS",
+        "00BDDDA000",
+        "000000A000",
+        "000000A000",
+        "0000000000",
+        "0000000000"]
+
+for line in board:
+    print line
+
+# Now have the main loop, alternating whether its our
+# turn or not
+
+guessno = 0
+while True:
+    if myturn:
+        guessno += 1
+        # we need to send a guess
+        guessx = guessno % 10
+        guessy = guessno // 10
+        logging.debug("My guess: (%d, %d)", guessx, guessy)
+        print "{},{}".format(guessx, guessy)
+    
+        # and now recieve what happened
+        data = raw_input()
+        logging.debug("Got %r", data)
+
+        myturn = False
+    else:
+        # if it isn't our turn, we just read our opponents
+        # guess
+        data = raw_input()
+        logging.debug("got opponent guess: %r", data)
+        myturn = True
+
+
+
