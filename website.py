53ec284cc1737ee655845964b1edcf787d6f78a9
Alex Alemi <alexalemi@gmail.com>
2015-07-18 15:22:23 -0700

    with first try at games

--- website.py
+++ website.py
@@ -23,6 +23,15 @@ env = Environment(loader=FileSystemLoader(TEMPLATES_PATH))
 
 leaderboard_row = namedtuple('row', 'rank name exposure mean sigma wins losses')
 
+def get_games():
+    records = glob.glob(os.path.join(RESULTS_PATH, "*.json"))
+    games = []
+    for record in records:
+        games.append(int(os.path.basename(record).split(".")[0]))
+    return games
+        
+games = get_games()
+
 def collect_errors():
     logging.info("Collecting errored games")
     records = glob.glob(os.path.join(RESULTS_PATH, "*.json")) 
@@ -41,6 +50,7 @@ errors = collect_errors()
 
 def make_index():
     """ Make the index page, which has the leaderboard """
+    logging.info("Making index page")
     template = env.get_template("index.html")
     with open("leaderboard.txt","r") as f:
         data = f.readlines()[2:]
@@ -50,6 +60,12 @@ def make_index():
 
 def make_games():
     """ Make the page for each game, as well as the games overview page """
+    logging.info("Making game pages")
+    template = env.get_template("game.html")
+    for game in games:
+        logging.info("Making game page for pk=%d", game)
+        with open(os.path.join(STATIC_PATH, "{}.html".format(game)), 'w') as f:
+            f.write(template.render(pk=game))
 
 def make_players():
     """ Make the page for all the players, and each player individually """
